c059ca05d07955f8823d696445631f4469c4e1e2
TahirDoDev <hmtahirhassan@gmail.com>
2025-04-03 15:41:51 +0500

    Quiz App

---
+++ quiz_app/lib/main_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+// MainScreen is a stateless widget that serves as the initial screen of the quiz app.
+// It displays a background color, an image, a title, and a button to start the quiz.
+
+class MainScreen extends StatelessWidget {
+  // Constructor for MainScreen. It takes a callback function `startQuiz`
+  // that is triggered when the "Start Quiz" button is pressed.
+  const MainScreen(this.startQuiz, {super.key});
+
+  // Callback function to handle the "Start Quiz" button press.
+  final void Function() startQuiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Set the background color of the screen to a purple shade.
+      backgroundColor: Color(0xFF560099),
+      body: Column(
+        // Center child widgets vertically within the column.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Display an image at the top of the screen with slight transparency.
+          Image.asset(
+            "assets/images/quiz.png",
+            color: const Color.fromARGB(150, 255, 255, 255),
+            width: 300, // Set the width of the image.
+          ),
+          SizedBox(height: 30), // Add vertical spacing between widgets.
+          // Display a title text below the image.
+          Center(
+            child: Text(
+              "Learn Flutter the fun way!", // Title text.
+              style: TextStyle(
+                color: Colors.white, // Set text color to white.
+                fontSize: 24, // Set font size.
+                fontWeight: FontWeight.normal, // Set font weight.
+              ),
+            ),
+          ),
+          SizedBox(height: 30), // Add more vertical spacing.
+          // Add a button that starts the quiz when pressed.
+          OutlinedButton.icon(
+            icon: Icon(
+              Icons.arrow_right_alt, // Add an arrow icon to the button.
+              color: Colors.white,
+            ),
+            onPressed: startQuiz, // Trigger the `startQuiz` function on press.
+            style: OutlinedButton.styleFrom(
+              foregroundColor:
+                  Colors.white, // Style the button with white text and icon.
+            ),
+            label: Text("Start Quiz"), // Button label text.
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/main_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  // Variable to keep track of the currently active screen in the app.
+  // Initially, the app starts with the 'main-screen' as the active screen.
+  var activeScreen = 'main-screen';
+
+  // Function to switch the active screen from the main screen to the questions screen.
+  // This function is triggered when the user interacts with the main screen (e.g., pressing a button).
+  // It updates the state of the widget, causing the UI to rebuild and display the QuestionsScreen.
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF560099), Color(0xFF9B1CFF)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          // The child widget dynamically changes based on the value of 'activeScreen'.
+          // If 'activeScreen' is set to 'main-screen', the MainScreen widget is displayed.
+          // The MainScreen widget takes a function (switchScreen) as an argument, which allows it to notify
+          // the parent widget (_QuizState) when the user interacts with it (e.g., pressing a button).
+          // When 'activeScreen' is set to 'questions-screen', the QuestionsScreen widget is displayed instead.
+          // This dynamic switching of widgets enables the app to transition between different screens
+          // based on user interaction, creating a seamless navigation experience.
+          child:
+              activeScreen == 'main-screen'
+                  ? MainScreen(switchScreen)
+                  : QuestionsScreen(),
+        ),
+      ),
+    );
+  }
+}
